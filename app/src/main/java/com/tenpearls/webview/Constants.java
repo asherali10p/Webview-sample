28b3fdcb30f4dd3c78f32f8bc56be1de2458e55e
Asher Ali <asher.ali@tenpearls.com>
2020-03-03 12:42:41 +0500

    initial commit

--- app/src/main/java/com/tenpearls/webview/Constants.java
+++ app/src/main/java/com/tenpearls/webview/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
     public static final CharSequence KEY_TELEPHONE_NUMBER = "tel:";
     public static final String KEY_CUSTOMER_CARE_NUMBER = "tel:03000639362";
     public static final String CODE_404 = "404";
-    public static String BASE_URL = "https://web.whatsapp.com/";
+    public static String BASE_URL = "https://www.google.com/";
 }
