552c3af046e38262cb8deece5a74d0ad2d1f8176
Asher Ali <asher.ali@tenpearls.com>
2020-03-03 12:29:35 +0500

    initial commit

--- app/src/main/java/com/tenpearls/webview/Constants.java
+++ app/src/main/java/com/tenpearls/webview/Constants.java
@@ -5,9 +5,5 @@ public class Constants {
     public static final CharSequence KEY_TELEPHONE_NUMBER = "tel:";
     public static final String KEY_CUSTOMER_CARE_NUMBER = "tel:03000639362";
     public static final String CODE_404 = "404";
-    public static final int PERMISSIONS_FOR_VOICE_CALL = 101;
-    public static final int PERMISSIONS_FOR_VIDEO_CALL = 102;
-    public static final String PERMISSIONS_ACTIVITY_NAME = "com.android.packageinstaller.permission.ui.GrantPermissionsActivity";
-
-    public static String BASE_URL = "https://sehatkahani-qa.pt-staging.com/?data=paQvAVM9wszDJ1GwmJKSbskGdkEBXcTTym4ADOjGQjbQCvzvDRpwhxWzXqRh9tTo7PveMSutIA2VjBGM%2BjHUySw4wNoD0T3Ze72pkGJUkxnWgL%2F4Aj6oUhJIt4YtWl7yjVTPHpi7f5NLmF9oq81LlJEZFrt%2B2OlA6RBJAAIidpUZnmf3aIxZjuVdwQNYu2raOOFZpGFdlK%2FF4kWPiDpx4cYsGEAKIBGvTonlZ7PJO%2BZKR5tfjyibtUoZvwNuyhyFv8YUsjwcToG1tNX1w64FYvvLajT%2Bdvv5KJr9MXZbMwuNuryC%2BdrG3Fq1NiWp6P8jTVs%2BgOcqldfqJhFWzG3nI7vp6YlUxKiYtI6FmSlQLui0bVz8XyIzDKZGMANRceWBUFedn7DumGEHVa9wqoiLYYlWpLV%2BA7ttDuqOpYQqCzGEEcCy3bs5D2Yt5JElUQz7SQ293bnuwkoCKjKzbE31EZt3DbVe7km2M9uwHpW8iNIpoJqDeM3vNvNYSd2R22gSmYCvALrT%2BHfvtR6%2B%2FIseMdv3PPTp3IVyZ9ZxW77E3qYZdn9dtohs7%2BdSEYurOV6A1RsSK1ONJizS29IqQO%2FuguqJBxkhKAGsi8fgNOcDfJ%2BOnZ8oAfF1F%2B8XJ0MIqbrZsb36gj56eia7ps56IpXpYyHhooJbh9ejInUy3l11JD8%3D";
+    public static String BASE_URL = "https://web.whatsapp.com/";
 }
